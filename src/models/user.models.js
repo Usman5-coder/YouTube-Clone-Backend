92be95aacf628078d230ee3493f5b984b0989427
usman_coder_5 <muhammadusmansubhani12@gmail.com>
2026-02-19 12:35:54 +0500

    password hashing with bcryot

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,51 +1,64 @@
-import mongoose, {Schema} from "mongoose"
+import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 const userSchema = new Schema(
-    {
-        username:{
-            type:String,
-            required:true,
-            lowercase:true,
-            unique:true,
-            index:true,
-            trim:true
-        },
-        email:{
-            type:String,
-            required:true,
-            lowercase:true,
-            unique:true,
-            trim:true
-        },
-        fullName:{
-            type:String,
-            required:true,
-            trim:true,
-            index:true
-        },        
-        avatar:{
-            type:String, //cloudinary URL
-            required:true,
-        },
-        coverimage:{
-            type:String,
-            required:true,
-        },
-        password:{
-            type:String,
-            required:[true, "Password is required!"],
-        },
-        refreshToken:{
-            type:String
-        },
-        watchHistory:[
-            {
-                type: Schema.Types.ObjectId,
-                ref: "Video"
-            }
-        ]
-
+  {
+    username: {
+      type: String,
+      required: true,
+      lowercase: true,
+      unique: true,
+      index: true,
+      trim: true,
     },
-    {timestamps:true})
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+      unique: true,
+      trim: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String, //cloudinary URL
+      required: true,
+    },
+    coverimage: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required!"],
+    },
+    refreshToken: {
+      type: String,
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
-export const User = mongoose.model("User", userSchema)
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+export const User = mongoose.model("User", userSchema);
