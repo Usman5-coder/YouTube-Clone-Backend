e4fd8d0f9339f943325a89964f4538770567bd05
usman_coder_5 <muhammadusmansubhani12@gmail.com>
2026-02-19 14:41:32 +0500

    file uploading basics with multer and cloudinary

--- src/app.js
+++ src/app.js
@@ -6,8 +6,8 @@ const app = express();
 
 app.use(cors({ origin: process.env.CORS_ORIGIN }));
 app.use(express.json({ limit: "16kb" }));
-app.use(express.urlencoded({ extended: true, limit:"16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
-
+app.use(cookieParser());
 
 export { app };
